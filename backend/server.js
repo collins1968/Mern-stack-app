bf1950fd06f9f90f5062919c82e47d07d15d0f31
Collins Mwendwa <collinsmwendwa1968@gmail.com>
2023-01-30 19:00:25 +0300

    created task route and middleware

--- backend/server.js
+++ backend/server.js
@@ -3,14 +3,28 @@ const express = require("express");
 //const { default: mongoose } = require("mongoose");
 //const connectDb = require("./config/connectDB");
 const mongoose = require("mongoose");
+const Task = require("./model/taskModel");
+const taskRoute = require("./Routes/taskRoute")
 
 const app = express()
 
+//middleware
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+app.use(taskRoute);
+
+/*
+const logger = (req, res, next) => {
+    console.log("middleware ran");
+    console.log(req.method);
+    next();
+};
+*/
 //route
 app.get("/", (req, res) => {
     res.send("<h1>home page</h1>"); 
 });
-
+ 
 
 const PORT = process.env.PORT || 5000
 
