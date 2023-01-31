9780e8b62c5f8fc08d1cb568fbf4e0c074ca18e9
Collins Mwendwa <collinsmwendwa1968@gmail.com>
2023-01-31 12:48:22 +0300

    added controllers

--- backend/Routes/taskRoute.js
+++ backend/Routes/taskRoute.js
@@ -1,26 +1,13 @@
 const express = require("express");
+const { createTask, getTask } = require("../controllers/taskControllers");
 const Task = require("../model/taskModel");
 const router = express.Router()
 
 // post the task to the database
-router.post("/api/tasks", async (req, res) => {
-    try {
-       const task = await Task.create(req.body);
-       res.status(200).json(task); 
-    } catch (error) {
-        res.status(500).json({msg: error.message});     
-    }
-});
+router.post("/api/tasks", createTask);
 
 //get the task from the database
-router.get("/api/tasks", async (req, res) => {
-    try {
-        const tasks = await Task.find();
-        res.status(200).json(tasks);
-    } catch (error) {
-        res.status(500).json({msg: error.message});
-    }
-});
+router.get("/api/tasks", getTask );
 
 
 module.exports = router
